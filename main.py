58d0ef8f1ef069668a40bb8c87d24240908e9108
Oliver Köhler <olk.git@posteo.eu>
2025-04-21 23:26:38 +0200

    removed everything for fresh start with PyQt6

--- main.py
+++ main.py
@@ -1,66 +1,10 @@
-import json
-import locale
 import sys
-from pathlib import Path
-
-import qdarktheme
-from PySide6 import QtCore
-from PySide6.QtCore import QTranslator, QLocale
-from PySide6.QtWidgets import QApplication, QWidget
-
-from logic.config import properties
-from logic.database import init_database
-from updates.update_helper import execute_updates
-from views.mainView import MainWindow
-
-userHome = Path.home()
-configDirectory = Path.joinpath(userHome, ".pyIM")
-configFile = Path.joinpath(configDirectory, "config.json")
-
-
-def load_translations():
-    translator = QTranslator(app)
-    path = './translations'
-    if properties.locale_index == 1:
-        translator.load(QLocale(QLocale.German, QLocale.Germany), 'base', '_', path)
-        locale.setlocale(locale.LC_TIME, "de_DE.utf8")
-        app.installTranslator(translator)
-
-
-def load_theme():
-    if properties.theme_index == 0:
-        qdarktheme.setup_theme()
-    elif properties.theme_index == 1:
-        qdarktheme.setup_theme("light")
-    elif  properties.theme_index == 2:
-        qdarktheme.setup_theme("auto")
-
-
-def write_config_file():
-    config_dict = {
-        "history": []
-    }
-    config = json.dumps(config_dict, indent=4)
-    with open(configFile, "w") as f:
-        f.write(config)
-        f.close()
-
+from PyQt6.QtWidgets import QApplication, QWidget
 
 if __name__ == "__main__":
-    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
-
-    app = QApplication()
-
-    properties.load_config_file()
-
-    init_database()
-
-    load_translations()
-    load_theme()
-
-    execute_updates()
-
-    form = QWidget(None)
-    MainWindow(form)
-    form.show()
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.setWindowTitle("Toolbox UI")
+    window.resize(400, 300)
+    window.show()
     sys.exit(app.exec())
--- updates/update_functions.py
+++
@@ -1,9 +0,0 @@
-from logic.config import properties
-from logic.database import persist_item
-
-
-def dummy_update():
-    """Example of an update function"""
-    s = properties.open_session()
-    # Update code to be inserted
-    s.commit()
